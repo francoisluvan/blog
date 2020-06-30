511ad323e8b14ecdae935477a16dbea6f963db28
François Lu Van <61683553+francoisluvan@users.noreply.github.com>
2020-06-30 14:17:37 +0200

    Delete config - online.php

--- admin/config - online.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-//Connexion à la base de données
-$link = mysqli_connect("bisonfgadmin.mysql.db", "bisonfgadmin","Tarsi0701", "bisonfgadmin") or die ("Impossible de se connecter: ".mysql_error());
-
-?>
